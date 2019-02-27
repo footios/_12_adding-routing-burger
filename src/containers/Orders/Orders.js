b08b3d7b9011ab99f39297264ce97de4dba1d202
Fotis Tsakiris <footios76@gmail.com>
2019-02-27 14:48:46 +0200

    254. Fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 
 import Order from '../../components/Order/Order'
+import axios from "../../axios-orders";
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
-    state = {  }
+    state = { 
+        orders: [],
+        loading: true
+     }
+    
+    componentDidMount() {
+        // From orders we fetch an object.
+        axios.get('/orders.json')
+            .then(res => {
+                console.log(res.data) // Why is 'data' what we get back from Firebase?
+                //convert the object into an array:
+                    const fetchedOrders = []
+                for (const key in res.data) {
+                    if (res.data.hasOwnProperty(key)) {
+                       fetchedOrders.push({...res.data[key], id: key});
+                        
+                    }
+                }
+                this.setState({loading: false, orders: fetchedOrders})
+            })
+            .catch(err => {
+                this.setState({loading: false})
+            })
+    }
+    
     render() { 
         // I obviously want to output multiple orders, 
         // actually as many orders as needed and the orders I need to output
@@ -17,4 +43,4 @@ class Orders extends Component {
     }
 }
  
-export default Orders;
+export default withErrorHandler(Orders, axios);
